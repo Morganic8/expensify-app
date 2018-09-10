9e896b0153a346b7a26e60442cad0b37502cdbe5
Morgan Atwood <atwood.morganj@gmail.com>
2018-09-10 12:55:51 -0700

    add startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -65,7 +65,15 @@ export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
-})
+});
+
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then( () => {
+            dispatch(editExpense(id, updates));
+        })
+    }
+}
 
 
 //SET EXPENSES
